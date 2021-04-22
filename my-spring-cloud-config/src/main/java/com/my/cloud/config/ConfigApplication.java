1cfe16bdcca14de4a00e1bee5cac7ae761528dfd
liuzhq <hdff0023@163.com>
2021-04-22 23:50:53 +0800

    1111

--- my-spring-cloud-config/src/main/java/com/my/cloud/config/ConfigApplication.java
+++ my-spring-cloud-config/src/main/java/com/my/cloud/config/ConfigApplication.java
@@ -7,7 +7,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Title: 配置中心启动类
- * @Description:
+ * @Description: Http 请求地址和资源文件映射
+ *                    1.http://ip:port/{application}/{profile}[/{label}
+ *                    2.http://ip:port/{application}-{profile}.yml
+ *                    3.http://ip:port/{label}/{application}-{profile}.yml
+ *                    4.http://ip:port/{application}-{profile}.properties
+ *                    5.http://ip:port/{label}/{application}-{profile}.properties
  * @date 2021年4月22日
  * @author liuzhq
  * @version V1.0
