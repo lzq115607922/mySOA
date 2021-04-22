c5ab5b3e773944e26ee399622d52b3b89dd85f70
liuzhq <hdff0023@163.com>
2021-04-22 21:18:07 +0800

    分布式配置中心

--- my-spring-cloud-config/src/main/java/com/my/cloud/config/ConfigApplication.java
+++ my-spring-cloud-config/src/main/java/com/my/cloud/config/ConfigApplication.java
@@ -1,10 +1,22 @@
-package com.my.cloud.config;/**
- * @Title:  
- * @ClassName: 
- * @Description:  
- * @author liuzhq 
- * @date 2021年4月22日 
+package com.my.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Title: 配置中心启动类
+ * @Description:
+ * @date 2021年4月22日
+ * @author liuzhq
  * @version V1.0
  */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
