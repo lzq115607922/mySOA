45fcec1b50b3105cf71a7d8dc242345cf55a36bc
liuzhq <hdff0023@163.com>
2021-04-22 21:14:38 +0800

    服务提供者

--- my-spring-cloud-service-admin/src/main/java/com/my/cloud/service/admin/ServiceAdminApplication.java
+++ my-spring-cloud-service-admin/src/main/java/com/my/cloud/service/admin/ServiceAdminApplication.java
@@ -1,4 +1,4 @@
-package com.my.cloud.service.admin.controller;
+package com.my.cloud.service.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- my-spring-cloud-service-admin/src/main/java/com/my/cloud/service/admin/controller/AdminController.java
+++ my-spring-cloud-service-admin/src/main/java/com/my/cloud/service/admin/controller/AdminController.java
@@ -1,2 +1,26 @@
-package com.my.cloud.service.admin.controller;public class AdminController {
+package com.my.cloud.service.admin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 服务类：获取端口
+ * @ClassName: AdminController
+ * @Description: TODO
+ * @author liuzhq
+ * @date 2021年4月21日
+ */
+@RestController
+public class AdminController {
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping(value = "hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam(value = "message") String message) {
+        // %s 是占位符
+        return String.format("Hi, your message is : %s port : %s", message, port);
+    }
 }
