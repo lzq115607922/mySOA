3744092e07ac679e3a8d049c8596e622e2225678
liuzhq <hdff0023@163.com>
2021-04-22 23:40:49 +0800

    1

--- my-spring-cloud-zuul/src/main/java/com/my/cloud/zuul/ZuulApplication.java
+++ my-spring-cloud-zuul/src/main/java/com/my/cloud/zuul/ZuulApplication.java
@@ -9,12 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @author liuzhq
  * @Title: 网关启动服务
  * @ClassName: ZuulApplication
- * @Description: Http 请求地址和资源文件映射
- *                  1.http://ip:port/{application}/{profile}[/{label}
- *                  2.http://ip:port/{application}-{profile}.yml
- *                  3.http://ip:port/{label}/{application}-{profile}.yml
- *                  4.http://ip:port/{application}-{profile}.properties
- *                  5.http://ip:port/{label}/{application}-{profile}.properties
+ * @Description:
  * @date 2020年4月22日
  * @version V1.0
  */
